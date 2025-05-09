bc039b54542afba619211c20082c1293c34d94df
enochkaba12345 <ekabatantshi@gmail.com>
2025-05-09 16:10:02 +0100

    Update JdbcConfig.java

--- src/main/java/com/sysgepecole/demo/Security/JdbcConfig.java
+++ src/main/java/com/sysgepecole/demo/Security/JdbcConfig.java
@@ -19,7 +19,7 @@ public class JdbcConfig {
 	    public DataSource dataSource() {
 	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 	        dataSource.setDriverClassName("org.postgresql.Driver"); // Use the PostgreSQL driver class
-	        dataSource.setUrl("jdbc:postgresql://dpg-d0e7jf9r0fns73crcibg-a.oregon-postgres.render.com:5432/bddsysgespecole"); // Replace with your PostgreSQL database URL
+	        dataSource.setUrl("jdbc:postgresql://dpg-d0esmc95pdvs73b21u4g-a.oregon-postgres.render.com:5432/bddsysgespecole"); // Replace with your PostgreSQL database URL
 	        dataSource.setUsername("bddsysgespecole_user"); // Replace with your PostgreSQL username
 	        dataSource.setPassword("L84pxZ3P6P0BCUekj5ffsfoQpZ51SWM5"); // Replace with your PostgreSQL password
 	        return dataSource;
