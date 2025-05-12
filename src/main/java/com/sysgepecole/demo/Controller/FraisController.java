c6e8cd635f055ade5f124eae29a075f02aef7330
enochkaba12345 <ekabatantshi@gmail.com>
2025-05-12 16:54:03 +0100

    Update FraisController.java

--- src/main/java/com/sysgepecole/demo/Controller/FraisController.java
+++ src/main/java/com/sysgepecole/demo/Controller/FraisController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.HashMap;
+
 
 import com.sysgepecole.demo.Dto.FraisDto;
 import com.sysgepecole.demo.Models.Frais;
