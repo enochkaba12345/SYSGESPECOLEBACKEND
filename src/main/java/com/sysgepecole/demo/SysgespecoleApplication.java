6c6ecf4dea489ab173f1fc7cb99857a58b987bad
enochkaba12345 <ekabatantshi@gmail.com>
2025-05-15 11:26:43 +0100

    Update SysgespecoleApplication.java

--- src/main/java/com/sysgepecole/demo/SysgespecoleApplication.java
+++ src/main/java/com/sysgepecole/demo/SysgespecoleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sysgepecole.demo")
 public class SysgespecoleApplication {
 
 	public static void main(String[] args) {
