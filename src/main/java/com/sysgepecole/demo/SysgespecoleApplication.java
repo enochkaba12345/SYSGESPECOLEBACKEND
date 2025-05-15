ae5e3021ff2263249f8401be9371a5d6f4fbc610
enochkaba12345 <ekabatantshi@gmail.com>
2025-05-15 14:22:56 +0100

    Update SysgespecoleApplication.java

--- src/main/java/com/sysgepecole/demo/SysgespecoleApplication.java
+++ src/main/java/com/sysgepecole/demo/SysgespecoleApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.ComponentScan;
+
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.sysgepecole.demo", "com.sysgepecole.demo.Security"})
 public class SysgespecoleApplication {
 
 	public static void main(String[] args) {
