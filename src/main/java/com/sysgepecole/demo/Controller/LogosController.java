8d8c11875675127a02daf8538e473125bb6d00a2
enochkaba12345 <ekabatantshi@gmail.com>
2025-05-15 10:02:30 +0100

    Update LogosController.java

--- src/main/java/com/sysgepecole/demo/Controller/LogosController.java
+++ src/main/java/com/sysgepecole/demo/Controller/LogosController.java
@@ -3,6 +3,7 @@ package com.sysgepecole.demo.Controller;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
